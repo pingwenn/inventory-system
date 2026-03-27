708e66ddfc1b5d0a0811414a0bfc137338e0ea04
Wen <pingwen851022@gmail.com>
2026-03-27 21:43:59 +0800

    feat: 實作查詢所有商品(GET)API

--- src/main/java/com/portfolio/inventory/controller/ProductController.java
+++ src/main/java/com/portfolio/inventory/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.portfolio.inventory.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +22,7 @@ public class ProductController {
 	private ProductService productService;
 	
 	/* *
-	 * 如果有人對api發送請求，立刻呼叫createProduct方法來處理
+	 * 如果有人對api發送請求 立刻呼叫createProduct方法來處理
 	 * @RequestBody 把前端傳來的JSON文字 自動轉解析並轉換成物件
 	 * createProduct方法 把轉換好的物件交給service 讓service呼叫Repository存進資料庫後
 	 * service存好資料庫後 產生帶有新ID的實體回傳給前端(springBoot會自動再次轉換成JSON格式)
@@ -27,5 +30,13 @@ public class ProductController {
 	@PostMapping 
 	public Product createProduct(@RequestBody Product product) {
 		return productService.addProduct(product);
+	}
+	
+	/* *
+	 * 發送請求時 呼叫getAllProducts
+	 * */
+	@GetMapping
+	public List<Product> getAllProducts(){
+		return productService.getAllProducts();
 	}
 }
--- src/main/java/com/portfolio/inventory/service/ProductService.java
+++ src/main/java/com/portfolio/inventory/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.portfolio.inventory.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +11,27 @@ import com.portfolio.inventory.repository.ProductRepository;
 @Service	// 宣告標記它是一個服務
 public class ProductService {
 	
-	// 依賴注入 允許spring loC容器自動將符合類型的Bean裝配到標注的成員變數、Setter方法或建構子中
-	// 把依賴管理交給容器，簡化代碼降低物件間的耦合度
+/* *
+ * 依賴注入 允許spring loC容器自動將符合類型的Bean裝配到標注的成員變數、Setter方法或建構子中
+ * 把依賴管理交給容器，簡化代碼降低物件間的耦合度
+ * */
 	@Autowired	
 	private ProductRepository productRepository;
 	
+	/* *
+	 * 新增商品
+	 * */
 	public Product addProduct (Product product){
 		return productRepository.save(product);
 		
+	}
+	
+	/* *
+	 * 查詢商品
+	 * 執行方法時 呼叫productRepository執行SQL語法 
+	 * 撈出所有資料交給Controller商品實體List
+	 * */
+	public List<Product> getAllProducts(){
+		return productRepository.findAll();
 	}
 }
